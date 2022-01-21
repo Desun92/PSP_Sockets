49db936c577b5be432d32113cdd7424eec3db645
alumnoA <adri.villa92@gmail.com>
2022-01-21 13:44:46 +0100

    En progreso

---
+++ Sockets_PSP/src/castro/adrian/ejercicio2/ServidorSocketStream.java
@@ -0,0 +1,45 @@
+package castro.adrian.ejercicio2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorSocketStream {
+	public static void main(String[] args) {
+		int contador = 0;
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket();
+			InetSocketAddress addr = new InetSocketAddress("localhost",6666);
+			serverSocket.bind(addr);
+			
+			while(true) {
+				Socket socket = serverSocket.accept();
+				contador++;
+				
+				//InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				
+				os.write(Integer.toString(contador).getBytes());
+				socket.close();
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(serverSocket!=null) {
+				try {
+					serverSocket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+}
---
+++ Sockets_PSP/src/castro/adrian/ejercicio5/ClienteSocketDatagram.java
@@ -0,0 +1,36 @@
+package castro.adrian.ejercicio5;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
+public class ClienteSocketDatagram {
+
+	public static void main(String[] args) {
+		
+		try (DatagramSocket datagramSocket = new DatagramSocket()) {
+			
+			InetSocketAddress addr = new InetSocketAddress("192.168.106.16",7777);
+			datagramSocket.setReuseAddress(true);
+			datagramSocket.bind(addr);
+			
+			String mensaje = "Hola";
+			InetSocketAddress direccionDestino = new InetSocketAddress("192.168.106.71",5555);
+			DatagramPacket datagrama = new DatagramPacket(mensaje.getBytes(),mensaje.getBytes().length,direccionDestino);
+			datagramSocket.send(datagrama);
+			System.out.println("Envia mensaje -> "+mensaje);
+			
+			byte[] respuesta = new byte[25];
+			DatagramPacket datagramaRespuesta = new DatagramPacket(respuesta,respuesta.length);
+			datagramSocket.receive(datagramaRespuesta);
+			System.out.println("Respuesta -> "+new String(respuesta));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
